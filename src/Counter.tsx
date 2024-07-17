ecfd24169b8aabc33447f2a63c03f95aef9fdd6a
Tetyana2015 <tgoncharenko2013@gmail.com>
2024-07-17 16:35:32 -0700

    useReducer was implemented

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,20 +1,52 @@
-import {ReactNode, useState} from 'react'
+import {ChangeEvent, ReactNode, useReducer} from 'react'
+
+const initState = { count: 0, text: '' }
+
+const enum REDUCER_ACTION_TYPE {
+    INCREMENT,
+    DECREMENT,
+    NEW_INPUT
+}
+
+type ReducerAction = {
+    type: REDUCER_ACTION_TYPE,
+    payload?: string
+}
+const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
+    switch (action.type) {
+        case REDUCER_ACTION_TYPE.INCREMENT:
+            return { ...state, count: state.count + 1 }
+        case REDUCER_ACTION_TYPE.DECREMENT:
+            return { ...state, count: state.count - 1 }
+        case REDUCER_ACTION_TYPE.NEW_INPUT:
+            return { ...state, text: action.payload ?? '' }
+        default:
+            throw new Error()
+    }
+}
 
 type ChildrenType = {
     children: (num: number) => ReactNode
 }
 
 const Counter = ({ children }: ChildrenType) => {
-    const [count, setCount] = useState<number>(1)
+    const [state, dispatch] = useReducer(reducer, initState)
 
-    const increment = () => setCount(prev => prev + 1)
-    const decrement = () => setCount(prev => prev - 1)
+    const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT })
+    const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT})
+    const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: REDUCER_ACTION_TYPE.NEW_INPUT,
+            payload: e.target.value})
+    }
 
     return (
         <>
-            <h1>{ children(count) }</h1>
+            <h1>{ children(state.count) }</h1>
             <button onClick={() => increment()}>+</button>
             <button onClick={() => decrement()}>-</button>
+            <input type="text" onChange={handleTextInput} value={state.text} />
+            <h2>{state.text}</h2>
         </>
     )
 }
